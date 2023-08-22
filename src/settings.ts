1ea6656b529041cc3b8e1cd5306613c188f8a740
YU <111847207+YU000jp@users.noreply.github.com>
2023-08-22 21:42:55 +0900

    plugin settings

--- src/settings.ts
+++ src/settings.ts
@@ -6,19 +6,15 @@ import { t } from 'logseq-l10n';
 export const settingsTemplate: SettingSchemaDesc[] = [
     {
         key: "headingAddLocalizeDayOfWeek",
-        title: t(//強制的に日付フォーマットを変更する
-            "Force display of date format"
-        ),
+        title: t("Switch to display of the selected date format"),
         type: "heading",
         default: "",
         //グラフには影響を与えない
-        description: "*This setting does not affect the graph or files*",
+        description: t("*This setting does not affect the graph or files*"),
     },
     {//Journal links date format
         key: "booleanJournalLinkDateFormat",
-        title: t(
-            "Enable selected date format"
-        ),
+        title: t("Enable selected date format"),
         type: "boolean",
         default: false,
         //グラフには影響を与えない
@@ -26,9 +22,7 @@ export const settingsTemplate: SettingSchemaDesc[] = [
     },
     {//date format
         key: "dateFormat",
-        title: t(
-            "Journal link: Select date format  (if the above option is enabled)"
-        ),
+        title: t("Select date format  (if the above option is enabled)"),
         type: "enum",
         //<option>E, MM/dd/yyyy</option><option>E, dd-MM-yyyy</option><option>E, dd.MM.yyyy</option><option>E, yyyy/MM/dd</option><option>EEE, MM/dd/yyyy</option><option>EEE, dd-MM-yyyy</option><option>EEE, dd.MM.yyyy</option><option>EEE, yyyy/MM/dd</option><option>EEEE, MM/dd/yyyy</option><option>EEEE, dd-MM-yyyy</option><option>EEEE, dd.MM.yyyy</option><option>EEEE, yyyy/MM/dd</option><option>MM-dd-yyyy</option><option>MM/dd/yyyy</option><option>MMM do, yyyy</option><option>MMMM do, yyyy</option><option>MM_dd_yyyy</option><option>dd-MM-yyyy</option><option>do MMM yyyy</option><option>do MMMM yyyy</option><option>yyyy-MM-dd</option><option>yyyy-MM-dd EEEE</option><option>yyyy/MM/dd</option><option>yyyyMMdd</option><option>yyyy_MM_dd</option><option>yyyy年MM月dd日</option><
         enumChoices: [
@@ -57,16 +51,14 @@ export const settingsTemplate: SettingSchemaDesc[] = [
     },
     {//日付フォーマットのデモンストレーション
         key: "loadDateFormatDemo",
-        title: "All date format demo",
+        title: t("All date format demo"),
         type: "boolean",
         default: false,
         description: "Open the dialog on click",
     },
     {//booleanRelativeTime
         key: "booleanRelativeTime",
-        title: t(
-            "Display relative time on hover the journal link"
-        ),
+        title: t("Display relative time on hover the journal link"),
         type: "boolean",
         default: true,
         description: "default: `true`",
@@ -74,28 +66,22 @@ export const settingsTemplate: SettingSchemaDesc[] = [
 
     {
         key: "headingAddLocalizeDayOfWeek",
-        title: t(
-            "Localized day of the week (except `Localize`)"
-        ),
+        title: t("Localized day of the week (except `Localize`)"),
         type: "heading",
         default: "",
         //グラフには影響を与えない
-        description: "*This setting does not affect the graph or files*",
+        description: t("*This setting does not affect the graph or files*"),
     },
     {//Localizeを除く
         key: "booleanJournalLinkLocalizeDayOfWeek",
-        title: t(
-            "If the day of the week is included in user date format, localize the day of the week in the date link"
-        ),
+        title: t("If the day of the week is included in user date format, localize the day of the week in the date link"),
         type: "boolean",
         default: true,
         description: "default: `true`",
     },
     {//Localizeを除く
         key: "booleanJournalLinkAddLocalizeDayOfWeek",
-        title: t(
-            "If the day of the week is not included in user date format, add the localized day of the week to the date link"
-        ),
+        title: t("If the day of the week is not included in user date format, add the localized day of the week to the date link"),
         type: "boolean",
         default: true,
         description: "default: `true`",
