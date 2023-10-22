3320e2570d1805763b323f82baf1893e749295d3
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-22 12:21:36 +0900

    feat(Full Translations): af, de, es, fr, id, it, ko, nb-NO, nl, pl, pt-BR pt-PT, ru, sk, tr, uk, zh-CN, zh-Hant"

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,25 @@ import '@logseq/libs' //https://plugins-doc.logseq.com/
 import { settingsTemplate } from './settings'
 import { setup as l10nSetup } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
 import ja from "./translations/ja.json"
+import af from "./translations/af.json"
+import de from "./translations/de.json"
+import es from "./translations/es.json"
+import fr from "./translations/fr.json"
+import id from "./translations/id.json"
+import it from "./translations/it.json"
+import ko from "./translations/ko.json"
+import nbNO from "./translations/nb-NO.json"
+import nl from "./translations/nl.json"
+import pl from "./translations/pl.json"
+import ptBR from "./translations/pt-BR.json"
+import ptPT from "./translations/pt-PT.json"
+import ru from "./translations/ru.json"
+import sk from "./translations/sk.json"
+import tr from "./translations/tr.json"
+import uk from "./translations/uk.json"
+import zhCN from "./translations/zh-CN.json"
+import zhHant from "./translations/zh-Hant.json"
+
 import { journalLink } from './journalLink'
 import { AppUserConfigs, LSPluginBaseInfo } from '@logseq/libs/dist/LSPlugin.user'
 import { openStartWindow } from './demoDateFormat'
@@ -10,7 +29,11 @@ import { openStartWindow } from './demoDateFormat'
 /* main */
 const main = () => {
   (async () => {
-    await l10nSetup({ builtinTranslations: { ja } })
+    await l10nSetup({
+      builtinTranslations: {//Full translations
+        ja, af, de, es, fr, id, it, ko, "nb-NO": nbNO, nl, pl, "pt-BR": ptBR, "pt-PT": ptPT, ru, sk, tr, uk, "zh-CN": zhCN, "zh-Hant": zhHant
+      }
+    })
     /* user settings */
     logseq.useSettingsSchema(settingsTemplate())
     if (!logseq.settings!.firstLoad) {
